d0596fdc61e41bd1a6874b26ac1f8bb67ecd55a1
DengBinbin <928024300@qq.com>
2016-06-27 10:24:18 +0800

    6.27

--- ML_zhou/4.3_DT.py
+++ ML_zhou/4.3_DT.py
@@ -5,7 +5,7 @@ import codecs
 import operator
 from math import log
 from treePlotter import *
-import matplotlib.pyplot as plt
+
 
 
 def calcShannonEnt(dataSet):
@@ -22,7 +22,7 @@ def calcShannonEnt(dataSet):
         shannonEnt -= prob * log(prob,2) #log base 2
     return shannonEnt
 
-#将数据按照axis的值为value去进行划分
+#将离散属性数据按照axis的值为value去进行划分
 def splitDataSet(dataSet,axis,value):
     returnMat = []
     for data in dataSet:
@@ -30,34 +30,81 @@ def splitDataSet(dataSet,axis,value):
             returnMat.append(data[:axis]+data[axis+1:])
     return returnMat
 
-'''
-如何选取最佳的切分点呢？一个很显然的思路，对于数据可能的切分方式，每一种都计算一次熵降，看看哪个最大就选择哪个
-params:base_ent原始切分前的数据集的熵
-ent_des熵降
-split_ent切分数据集之后的熵
-'''
-def chooseBestFeatureToSplit(dataSet):
-    base_ent = calcShannonEnt(dataSet)
+# 对连续变量划分数据集，direction规定划分的方向，
+# 决定是划分出小于value的数据样本还是大于value的数据样本集
+def splitContinuousDataSet(dataSet, axis, value, direction):
+    retDataSet = []
+    for featVec in dataSet:
+        if direction == 0:
+            if featVec[axis] > value:
+                retDataSet.append(featVec[:axis] + featVec[axis + 1:])
+        else:
+            if featVec[axis] <= value:
+                retDataSet.append(featVec[:axis] + featVec[axis + 1:])
+    return retDataSet
 
-    num_features = len(dataSet[0])-1
-    infoGain = 0
-    ent_des = 0
+
+# 不同决策树区别在于如何确定最佳划分特征的选取方法不同
+def chooseBestFeatureToSplit(dataSet, labels):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
     bestFeature = -1
-    for i in range(num_features):
-        feature_list = [example[i] for example in dataSet]
-        #上面求得的特征表是有重复特征的，只需求出其中不同元素即可
-        feature_list = set(feature_list)
-        split_ent = 0
-        for value in feature_list:
-            subDataSet = splitDataSet(dataSet,i,value)
-            prob = len(subDataSet)/float(len(dataSet))
-            split_ent+=prob*calcShannonEnt(subDataSet)
-        ent_des = base_ent-split_ent
-
-        if ent_des>infoGain:
-            infoGain = ent_des
+    bestSplitDict = {}
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        # 对连续型特征进行处理 ,i代表第i个特征
+        if type(featList[0]) == type(0.0) or type(featList[0]) == type(0):
+            # 产生n-1个候选划分点
+            sortfeatList = sorted(featList)
+            splitList = []
+            for j in range(len(sortfeatList) - 1):
+                splitList.append((sortfeatList[j] + sortfeatList[j + 1]) / 2.0)
+            bestSplitEntropy = 10000
+            slen = len(splitList)
+            # 求用第j个候选划分点划分时，得到的信息熵，并记录最佳划分点
+            for j in range(slen):
+                value = splitList[j]
+                newEntropy = 0.0
+                subDataSet0 = splitContinuousDataSet(dataSet, i, value, 0)
+                subDataSet1 = splitContinuousDataSet(dataSet, i, value, 1)
+                prob0 = len(subDataSet0) / float(len(dataSet))
+                newEntropy += prob0 * calcShannonEnt(subDataSet0)
+                prob1 = len(subDataSet1) / float(len(dataSet))
+                newEntropy += prob1 * calcShannonEnt(subDataSet1)
+                if newEntropy < bestSplitEntropy:
+                    bestSplitEntropy = newEntropy
+                    bestSplit = j
+                    # 用字典记录当前特征的最佳划分点
+            bestSplitDict[labels[i]] = splitList[bestSplit]
+            infoGain = baseEntropy - bestSplitEntropy
+            # 对离散型特征进行处理
+        else:
+            uniqueVals = set(featList)
+            newEntropy = 0.0
+            # 计算该特征下每种划分的信息熵
+            for value in uniqueVals:
+                subDataSet = splitDataSet(dataSet, i, value)
+                prob = len(subDataSet) / float(len(dataSet))
+                newEntropy += prob * calcShannonEnt(subDataSet)
+            infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
             bestFeature = i
+    # 若当前节点的最佳划分特征为连续特征，则将其以之前记录的划分点为界进行二值化处理
+    # 即是否小于等于bestSplitValue
+    if type(dataSet[0][bestFeature])==type(0.0) or type(dataSet[0][bestFeature]) == type(0):
+        bestSplitValue = bestSplitDict[labels[bestFeature]]
+        labels[bestFeature] = labels[bestFeature] + '<=' + str(bestSplitValue)
+        for i in range(shape(dataSet)[0]):
+            if dataSet[i][bestFeature] <= bestSplitValue:
+                dataSet[i][bestFeature] = 1
+            else:
+                dataSet[i][bestFeature] = 0
     return bestFeature
+
+
+
 
 '''
 这个函数的作用是返回字典中出现次数最多的value对应的key，也就是输入list中出现最多的那个值
@@ -74,39 +121,56 @@ def majorityCnt(classList):
 '''
 递归地去建树
 '''
-def createTree(dataSet,labels):
-    classList = [example[-1]for example in dataSet]
-    #classList:['yes', 'yes', 'no', 'no', 'no']
-    #如果classList里面全部都是它的第0个元素，即全部分为1类了
-    if classList.count(classList[0]) == len(classList):
+#主程序，递归产生决策树
+def createTree(dataSet,labels,data_full,labels_full):
+    classList=[example[-1] for example in dataSet]
+    if classList.count(classList[0])==len(classList):
         return classList[0]
-    #len(dataSet[0])是输入数据的列数，因为有一列是label，所以当len(dataSet)等于1的时候说明所有属性
-    if len(dataSet[0]) == 1:
+    if len(dataSet[0])==1:
         return majorityCnt(classList)
-    #一般情况
-    bestFeat = chooseBestFeatureToSplit(dataSet)
-    bestFeatLabel = labels[bestFeat]
-    myTree = {bestFeatLabel:{}}
+    bestFeat=chooseBestFeatureToSplit(dataSet,labels)
+    bestFeatLabel=labels[bestFeat]
+    myTree={bestFeatLabel:{}}
+    featValues=[example[bestFeat] for example in dataSet]
+    uniqueVals=set(featValues)
+    if type(dataSet[0][bestFeat]).__name__=='str':
+        currentlabel=labels_full.index(labels[bestFeat])
+        featValuesFull=[example[currentlabel] for example in data_full]
+        uniqueValsFull=set(featValuesFull)
     del(labels[bestFeat])
-    featValues = [example[bestFeat] for example in dataSet]
-    uniqueVals = set(featValues)
-    #每次找到的最佳切分点之后，按照最佳切分点
+    #针对bestFeat的每个取值，划分出一个子树。
     for value in uniqueVals:
-        subLabels = labels[:]
-        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+        subLabels=labels[:]
+        if type(dataSet[0][bestFeat]).__name__=='str':
+            uniqueValsFull.remove(value)
+        myTree[bestFeatLabel][value]=createTree(splitDataSet\
+         (dataSet,bestFeat,value),subLabels,data_full,labels_full)
+    if type(dataSet[0][bestFeat]).__name__=='str':
+        for value in uniqueValsFull:
+            myTree[bestFeatLabel][value]=majorityCnt(classList)
     return myTree
 
 # 读入csv文件数据
-input_path = "data/西瓜数据集3.csv"
-file = codecs.open(input_path,"r",'utf-8')
+# input_path = "data/西瓜数据集3.csv"
+# file = codecs.open(input_path,"r",'utf-8')
+#
+# filedata = [line.strip('\n').split(',') for line in file]
+# filedata = [[float(i) if '.' in i else i for i in row ] for row in filedata] # change decimal from string to float
+# dataSet = [row[1:] for row in filedata[1:]]
+#
+# labels = []
+# for label in filedata[0][1:-1]:
+#     labels.append(label)
+#
+# myTree = createTree(dataSet,labels)
+# #print myTree.keys()
+# createPlot(myTree)
 
-filedata = [line.strip('\n').split(',') for line in file]
-#filedata = [[float(i) if '.' in i else i for i in row ] for row in filedata] # change decimal from string to float
-dataSet = [row[1:] for row in filedata[1:]]
-
-labels = []
-for label in filedata[0][1:-1]:
-    labels.append(label)
-
-myTree = createTree(dataSet,labels)
-createPlot(myTree)
+df=pd.read_csv(u'data/西瓜数据集3.csv')
+data=df.values[:,1:].tolist()
+data_full=data[:]
+labels=df.columns.values[1:-1].tolist()
+labels_full=labels[:]
+myTree=createTree(data,labels,data_full,labels_full)
+print myTree
+#createPlot(myTree)
